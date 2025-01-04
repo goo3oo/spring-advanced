968ab66c3ae302dda3bfd9bc7abfa8ce98ba73eb
goo3oo <goo3oo0918@gmail.com>
2025-01-04 16:41:30 +0900

    level4: improve N+1 (@EntityGraph)

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -9,8 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
   // N+1 문제 방지를 방지하게 위해 @EntityGraph 선택 이유
-  // 1. 두 객체의 관계가 단순
-  // 2. findAll 이라는 조건이 단순
+  // 1. 두 객체의 관계가 단순함
+  // 2. findAll 이라는 조건이 단순함
   @EntityGraph(attributePaths = {"user"})
   Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
